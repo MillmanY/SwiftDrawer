d3b4ca25239f79a2bc23f60b87a34fc7cca3cc9b
Millman <millmanyang@gmail.com>
2019-07-01 16:13:11 +0800

    first commit

---
+++ Example/SwiftDrawer/Main/MainView2.swift
@@ -0,0 +1,23 @@
+//
+//  MainView2.swift
+//  SwiftDrawer_Example
+//
+//  Created by Millman on 2019/6/30.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView2 : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MainView2_Previews : PreviewProvider {
+    static var previews: some View {
+        MainView2()
+    }
+}
+#endif
---
+++ SwiftDrawer/Classes/DrawerControl.swift
@@ -0,0 +1,8 @@
+//
+//  DrawerControl.swift
+//  SwiftDrawer
+//
+//  Created by Millman on 2019/6/30.
+//
+
+import Foundation
